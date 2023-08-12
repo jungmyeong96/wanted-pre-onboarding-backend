b79ba6eb7d535706477db767504a028eca97d73c
한중명 <gkswndaud@gmail.com>
2023-08-13 01:28:18 +0900

    feat: Add swagger to project
    close #6

--- src/main/java/com/wanted/jungproject/hello/HelloController.java
+++ src/main/java/com/wanted/jungproject/hello/HelloController.java
@@ -1,13 +1,22 @@
 package com.wanted.jungproject.hello;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@RequestMapping(value = "/")
+@Tag(name = "hello", description = "hello API Document")
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+
+    @GetMapping("hello")
+    @Operation(summary = "기본 화면", description = "기본 화면을 출력합니다.", tags = {"View"})
     public String hello(Model model){
         model.addAttribute("hello","서버에서 보내준 값입니다");
         return "hello";
